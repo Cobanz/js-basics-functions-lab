de484f14d17facd1b42cb2852612426887e29efe
Charles Grant Nichols <grant@inkdots.com>
2021-04-06 13:52:47 -0500

    commit tree 6e9de6e43390cfbefff32d82a29fa354a10b0362

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+const hq = 42
+
+
+function distanceFromHqInBlocks(distance) {
+    return Math.abs(distance - hq) 
+
+}
+
+
+
+
+
+function distanceFromHqInFeet(distance) {
+    return Math.abs(hq - distance)*264
+
+}
+
+
+
+
+function distanceTravelledInFeet(north, south) {
+    return Math.abs(north - south)*264
+
+}
+
+
+
+
+
+function calculatesFarePrice(north, south) {
+    let block = Math.abs(north - south)*264;
+    if (block < 400) {return 0}
+    else if (block > 400 && block <=2000) {return .02 * (block - 400)}
+    else if (block > 2000 && block <=2500) {return 25}
+    else if (block > 2500) {return 'cannot travel that far'}
+}
